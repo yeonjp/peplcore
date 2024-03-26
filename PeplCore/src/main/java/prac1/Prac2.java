83a37a6427a26fa831ae6553fffcb187bfedcba0
Wonjung <gimpo5975@naver.com>
2024-03-26 19:51:40 +0900

    test파일 삭제

--- PeplCore/src/main/java/prac1/Prac2.java
+++
@@ -1,9 +0,0 @@
-package prac1;
-
-public class Prac2 {
-
-	public static void main(String[] args) {
-		System.out.println("김원중 확인이요~");
-	}//end of main
-
-}//end of class
--- PeplCore/src/main/java/prac1/prac3.java
+++
@@ -1,9 +0,0 @@
-package prac1;
-
-public class prac3 {
-
-	public static void main(String[] args) {
-		System.out.println("저도확인이용");
-	}
-
-}
